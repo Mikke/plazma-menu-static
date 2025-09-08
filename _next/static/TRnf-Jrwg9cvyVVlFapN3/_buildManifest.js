2b8153c8fb66243c032c754ee3e34bf58ea5e4a1
ml <ml@technotimes.org>
2025-09-08 18:48:08 +0700

    fix all

---
+++ _next/static/TRnf-Jrwg9cvyVVlFapN3/_buildManifest.js
@@ -0,0 +1,18 @@
+self.__BUILD_MANIFEST = {
+  "/": [
+    "./static/chunks/4eab0d3c931f2e50.js"
+  ],
+  "/_error": [
+    "./static/chunks/40a70128a48759a5.js"
+  ],
+  "__rewrites": {
+    "afterFiles": [],
+    "beforeFiles": [],
+    "fallback": []
+  },
+  "sortedPages": [
+    "/",
+    "/_app",
+    "/_error"
+  ]
+};self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
---
+++ _next/static/chunks/a77fc982816fb8f2.js
@@ -0,0 +1,6 @@
+__turbopack_load_page_chunks__("/_app", [
+  "static/chunks/d3ffb83c6d63b497.js",
+  "static/chunks/774bc9d6ffc1fe16.js",
+  "static/chunks/0179272221945cb1.css",
+  "static/chunks/turbopack-e774d4ba0184a090.js"
+])
